5cca69e74e4e08eaf75616be688babddf0217f17
James <james@jmew.ca>
2020-11-24 21:26:46 -0500

    test 0.1

---
+++ XpressionWS/Storage/Account.cs
@@ -0,0 +1,19 @@
+﻿
+namespace XpressionWS.Storage
+{
+    public class Account
+    {
+        private string _userName = "user";
+
+        public string UserName { get => _userName; set => _userName = value.Trim(); }
+        public string Password { get; set; } = "password";
+
+        public Account(string userName, string password)
+        {
+            UserName = userName;
+            Password = password;
+        }
+
+        public string LowerName { get => _userName.ToLower(); }
+    }
+}
